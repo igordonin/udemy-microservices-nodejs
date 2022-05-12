aee3b5c76bfd41ef7dba74714e5e7d7cc78be85b
igordonin <igordonin@gmail.com>
2022-05-12 17:38:20 -0300

    feat: handle comment created events and emit comment moderated

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -6,11 +6,22 @@ const app = express();
 app.use(bodyParser.json());
 
 
-app.post('/events', (req, res) => {
-  const { type } = req.body;
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
 
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        ...data,
+        status,
+      }
+    });
+  }
 
+  res.send({});
 });
 
 const PORT = 4003;
