d6a458a42b0674687a461382fcfc08953c8a746e
igordonin <igordonin@gmail.com>
2022-05-12 14:53:50 -0300

    feat: Emit events to service bus and receive events in the services

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,12 +12,10 @@ const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
   const postId = req.params.id;
-
-
   res.send(commentsByPostId[postId] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const postId = req.params.id;
 
   const commentId = randomBytes(4).toString('hex');
@@ -27,7 +26,21 @@ app.post('/posts/:id/comments', (req, res) => {
 
   commentsByPostId[postId] = comments;
 
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId,
+    }
+  })
+
   res.status(201).send(commentsByPostId[postId]);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Event Received', req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
