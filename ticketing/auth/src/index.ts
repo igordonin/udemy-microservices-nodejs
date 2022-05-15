11b035cfa50c29f1a3d4d8976b66eb6f7b8d2f01
igordonin <igordonin@gmail.com>
2022-05-15 09:18:40 -0300

    feat: scaffold routes and use them in the router

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/sign-in';
+import { signOutRouter } from './routes/sign-out';
+import { signUpRouter } from './routes/sign-up';
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
 
 app.listen(3000, () => {
   console.log('Auth listening on port 3000');
