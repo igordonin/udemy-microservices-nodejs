b48d2ed3833ba4a7893a255ccc93dd176d8ae5e1
igordonin <igordonin@gmail.com>
2022-05-18 20:56:57 -0300

    feat: add validation to the sign-in request

--- ticketing/auth/src/routes/sign-in.ts
+++ ticketing/auth/src/routes/sign-in.ts
@@ -1,9 +1,31 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+
+const validateRequest = (req: Request) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidationError(errors.array());
+  }
+};
 
 const router = express.Router();
 
-router.post('/api/users/sign-in', (req, res) => {
-  res.send('hi there');
-});
+router.post(
+  '/api/users/sign-in',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  (req: Request, res: Response) => {
+    validateRequest(req);
+
+    res.send('hi there');
+  }
+);
 
 export { router as signInRouter };
