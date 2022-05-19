20123461976ef0c8c5aead8cd715e91453c48ce7
igordonin <igordonin@gmail.com>
2022-05-19 18:41:54 -0300

    chore: configure jest for testing

---
+++ ticketing/auth/src/test/setup.ts
@@ -0,0 +1,25 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+let mongo: any;
+
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  collections.forEach(async (collection) => {
+    await collection.deleteMany({});
+  });
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
