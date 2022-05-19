fa604f1e1af18a4de4c0b410edfc6fac8450a779
igordonin <igordonin@gmail.com>
2022-05-19 15:24:08 -0300

    refactor: extract current user logic into a middleware and use it in current user route

---
+++ ticketing/auth/src/middlewares/current-user.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+const isJwtAbsentFromSession = (req: Request) => {
+  return !req.session?.jwt;
+};
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+export const currentUser = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (isJwtAbsentFromSession(req)) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session!.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+
+    req.currentUser = payload;
+  } catch (err) {}
+
+  next();
+};
--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,19 +1,14 @@
 import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/current-user', (req: Request, res: Response) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
+router.get(
+  '/api/users/current-user',
+  currentUser,
+  (req: Request, res: Response) => {
+    res.send({ currentUser: req.currentUser || null });
   }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    return res.send({ currentUser: null });
-  }
-});
+);
 
 export { router as currentUserRouter };
