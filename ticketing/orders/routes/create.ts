ddd381abb2c56f52a905cea47d11421476532ce5
igordonin <igordonin@gmail.com>
2022-05-26 18:06:46 -0300

    feat: add check ticket already reserved

--- ticketing/orders/routes/create.ts
+++ ticketing/orders/routes/create.ts
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth, validateRequest } from '@igordonin-org/ticketing-common';
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@igordonin-org/ticketing-common';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -16,8 +24,40 @@ router.post(
     .withMessage('Ticket id must be provided'),
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    verifyTicketIsNotReserved(req);
+
+    const ticket = Ticket.find({ _id: ticketId });
+    const order = Order.find({ ticket });
+
     res.send({});
   }
 );
 
 export { router as createOrderRouter };
+
+const verifyTicketIsNotReserved = async (req: express.Request) => {
+  const { ticketId } = req.body;
+
+  const ticket = await Ticket.findById(ticketId);
+
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  const existingOrder = await Order.findOne({
+    ticket,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  if (existingOrder) {
+    throw new BadRequestError(`Ticket ${ticket.title} is already reserved`);
+  }
+};
