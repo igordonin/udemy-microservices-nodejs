eddbaabec9dc08655ac60d6b9a29ae847a66e65e
igordonin <igordonin@gmail.com>
2022-05-26 15:42:57 -0300

    chore: set up orders service and deployment descriptors

---
+++ ticketing/orders/src/app.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@igordonin-org/ticketing-common';
+import { createTicketRouter } from './routes/create';
+import { findOneTicketRouter } from './routes/find-one';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(findOneTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ ticketing/orders/src/index.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+  [
+    'JWT_KEY',
+    'MONGO_URI',
+    'NATS_CLIENT_ID',
+    'NATS_URL',
+    'NATS_CLUSTER_ID',
+  ].forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`${key} must be defined`);
+    }
+  });
+
+  try {
+    await natsWrapper.connect({
+      clusterId: process.env.NATS_CLUSTER_ID!,
+      clientId: process.env.NATS_CLIENT_ID!,
+      url: process.env.NATS_URL!,
+    });
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Tickets listening on port 3000!');
+  });
+};
+
+start();
