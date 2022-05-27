645df20dd2ff6f5b05145661859e84a792232a27
igordonin <igordonin@gmail.com>
2022-05-27 07:42:06 -0300

    chore: set up testing

---
+++ ticketing/orders/src/test/setup.ts
@@ -0,0 +1,53 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  var signin: () => string[];
+}
+
+jest.setTimeout(60000);
+jest.mock('../nats-wrapper');
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'whatever';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  const payload = {
+    id,
+    email: 'test@test.com',
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const session = { jwt: token };
+  const sessionJsonString = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJsonString).toString('base64');
+
+  return [`session=${base64}`];
+};
