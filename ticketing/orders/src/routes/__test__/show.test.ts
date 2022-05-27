dc1cdfec945e0a6ab1884a895fbf5c8edd56069a
igordonin <igordonin@gmail.com>
2022-05-27 09:43:30 -0300

    feat: display a specific order to the client

---
+++ ticketing/orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,66 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import { app } from "../../app";
+import { Ticket, TicketDoc } from "../../models/ticket";
+
+const buildTicket = async (title: string) => {
+  const ticket = await Ticket.build({
+    title,
+    price: 10,
+  });
+
+  await ticket.save();
+  return ticket;
+};
+
+const buildOrder = async (cookie: string[], ticket: TicketDoc) => {
+  return await request(app).post("/api/orders").set("Cookie", cookie).send({
+    ticketId: ticket.id,
+  });
+};
+
+it("returns an error if order does not exist", async () => {
+  const id = new mongoose.Types.ObjectId();
+
+  await request(app)
+    .post(`/api/orders${id}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(404);
+});
+
+it("returns order from current user", async () => {
+  const ticket1 = await buildTicket("Concert 1");
+  const ticket2 = await buildTicket("Concert 2");
+
+  const user1 = global.signin();
+  const user2 = global.signin();
+
+  const { body: order1 } = await buildOrder(user1, ticket1);
+  const { body: order2 } = await buildOrder(user2, ticket2);
+
+  const response = await request(app)
+    .get(`/api/orders/${order2.id}`)
+    .set("Cookie", user2)
+    .send()
+    .expect(200);
+
+  expect(response.body.id).toEqual(order2.id);
+});
+
+it("not authorized for order from another user", async () => {
+  const ticket1 = await buildTicket("Concert 1");
+  const ticket2 = await buildTicket("Concert 2");
+
+  const user1 = global.signin();
+  const user2 = global.signin();
+
+  const { body: order1 } = await buildOrder(user1, ticket1);
+  const { body: order2 } = await buildOrder(user2, ticket2);
+
+  await request(app)
+    .get(`/api/orders/${order2.id}`)
+    .set("Cookie", user1)
+    .send()
+    .expect(401);
+});
--- ticketing/orders/src/routes/show.ts
+++ ticketing/orders/src/routes/show.ts
@@ -1,9 +1,32 @@
-import express, { Request, Response } from 'express';
+import {
+  currentUser,
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from "@igordonin-org/ticketing-common";
+import express, { Request, Response } from "express";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.get('/api/orders/:id', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:id",
+  currentUser,
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const order = await Order.findById(id).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
