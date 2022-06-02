aa6266ace9da841a893df0ad0bf693abc844947e
igordonin <igordonin@gmail.com>
2022-06-02 18:42:35 -0300

    refactor: pass the client and the current user forward as params to get initial props

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -12,16 +12,19 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
 };
 
 AppComponent.getInitialProps = async (appContext) => {
-  const { data } = await buildClient(appContext.ctx)
-    .get('/api/users/current-user')
-    .catch((err) => {
-      console.log(err);
-      return {};
-    });
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/current-user').catch((err) => {
+    console.log(err);
+    return {};
+  });
 
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      data.currentUser
+    );
   }
 
   return {
